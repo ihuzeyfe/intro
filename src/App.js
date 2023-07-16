e68787e4f184662f4d140762dce4fb236f466a76
İsmail Huzeyfe Çobanoğlu <129663858+ihuzeyfe@users.noreply.github.com>
2023-07-16 15:43:08 +0300

    ncopsulation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
+  let categoryInfo = {title:"Huzeyfe", baskaBirsey:"İstanbul"}
+  let productInfo = {title:"İsmail"}
   return (
-    <div >
-      <h3> Hello Darkness My Old Friend</h3>
-      <h2>Aynı Hiyerarşiden koyabiliriz, fakat en dışa 2 div koyamayız.</h2>
+    <div>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo}></CategoryList>
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo} ></ProductList>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/Navi.js
+++ src/Navi.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Navi extends Component {
   render() {
     return (
-      <div>Navi</div>
+      <div>
+        <h2> Navi Component</h2>
+      </div>
     )
   }
 }
